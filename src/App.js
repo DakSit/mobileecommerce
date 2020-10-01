c989f3d4d335ec1f9607d9d911f5d11fde3452c9
Dakota Sitton <dakotasitton@gmail.com>
2020-10-01 14:49:15 -0500

    Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { createAppContainer } from 'react-navigation';
+
+import AppNavigator from './AppNavigator';
+
+
+class App extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default App;
